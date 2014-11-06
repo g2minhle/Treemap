b36b713e7dd8d044f30b53371b068d23715f1614
Le Hoang <minhhoan@uf8bc127800b853f77f28.ant.amazon.com>
2014-11-06 09:57:52 -0800

    Add extra lib in case when install in case it needed

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 echo "Instal required libs"
+apt-get install python
+apt-get install python-tk
 apt-get install python-pygame
 cd ./PyGraphics-2.1
 python setup.py install --record file.txt
